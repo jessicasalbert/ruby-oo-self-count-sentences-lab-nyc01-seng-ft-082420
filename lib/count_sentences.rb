3fccefe8318ee504dc0ecd200c951bccbf9ad116
jessicasalbert <jessicarsalbert@gmail.com>
2020-08-26 17:06:06 -0400

    completed labs

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "."
   end
 
   def question?
-
+    self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!"
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences.filter { |word| word != "" }.count
   end
 end
